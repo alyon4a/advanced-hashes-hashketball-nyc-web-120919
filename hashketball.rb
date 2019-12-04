acc7c0603a1b0a49085cef41400fa826524651c2
Olena Matokhina <olena@matokhina.com>
2019-12-04 16:19:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -95,10 +95,12 @@ def big_shoe_rebounds
 end
 
 def most_points_scored 
-  game_data = game_hash
+  # game_data = game_hash
   
-  player_res = game_data[:home][:players].reduce {|memo, player| memo[:points] > player[:points] ? memo : player}
-  player_res = game_data[:away][:players].reduce (player_res) {|memo, player| memo[:points] > player[:points] ? memo : player}
+  # player_res = game_data[:home][:players].reduce {|memo, player| memo[:points] > player[:points] ? memo : player}
+  # player_res = game_data[:away][:players].reduce (player_res) {|memo, player| memo[:points] > player[:points] ? memo : player}
+  
+  player_res = find_player_with_highest_number(:points)
  
   player_res[:player_name]
 end
