4ed4daa62e65d533eb0fe6fec6fc5ebab5ccba9e
Olena Matokhina <olena@matokhina.com>
2019-12-04 16:28:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -122,7 +122,7 @@ def long_name_steals_a_ton?
   steal_player = find_player_with_highest_number(:steals)
   long_name_player = player_with_longest_name
   
-  return steal_player == long_name_player
+  return steal_player[:player_name] == long_name_player[:player_name]
 end
 
 def find_player_with_highest_number(player_hash_key)
